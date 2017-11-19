cb8f173e8ba5fd224d388b1a7952e241a93e08e7
bendaf <bendaf92@gmail.com>
2017-11-19 17:57:03 +0100

    Optmize edge_recombination for adjacency and path representation

--- edge_recombination.m
+++ edge_recombination.m
@@ -8,49 +8,53 @@
 
 
 function Offspring = edge_recombination(Parents, Repr)
-    cols = size(Parents, 2);
-	Offspring = zeros(1, cols);
     
-    Parents = conv_repr(Parents, Repr, 3);
-    edgeList = zeros(cols, 4);
+    edgeList = zeros(size(Parents, 2), 4);
+    if Repr == 1
+        edgeList(:,1:2) = create_edgeList_path(Parents(1,:));
+        edgeList(:,3:4) = create_edgeList_path(Parents(2,:));
+        Offspring = create_child_path(edgeList);
+    elseif Repr == 2
+        edgeList(:,1:2) = create_edgeList_adj(Parents(1,:));
+        edgeList(:,3:4) = create_edgeList_adj(Parents(2,:));
+        Offspring = create_child_adj(edgeList);
+    elseif Repr == 3
+        edgeList(:,1:2) = create_edgeList_ordinal(Parents(1,:));
+        edgeList(:,3:4) = create_edgeList_ordinal(Parents(2,:));
+        Offspring = create_child_ordinal(edgeList);
+    end
     
-    %first parent
-    L = 1:cols;
-	firstCity = L(Parents(1, 1));
-    previousCity = L(Parents(1, 1));
-    L(Parents(1, 1)) = [];
-	for t = 2:size(Parents,2)
-		edgeList(L(Parents(1,t)), 1) = previousCity;
-        edgeList(previousCity, 2)  = L(Parents(1,t));
-        previousCity = L(Parents(1,t));
-		L(Parents(1,t)) = [];
-        if size(L,2) == 0
+end
+% End of function
+
+function edgeList = create_edgeList_ordinal(parent)
+    gens = size(parent, 2); 
+    L = 1:gens;
+    edgeList = zeros(gens, 2);
+	firstCity = L(parent(1));
+    previousCity = L(parent(1));
+    L(parent(1)) = [];
+    for t = 2:gens
+		edgeList(L(parent(t)), 1) = previousCity;
+        edgeList(previousCity, 2)  = L(parent(t));
+        previousCity = L(parent(t));
+		L(parent(t)) = [];
+        if size(L, 2) == 0
             edgeList(firstCity, 1) = previousCity;
             edgeList(previousCity, 2) = firstCity;
         end
     end
-    
-    %second parent
-    L = 1:cols;
-	firstCity = L(Parents(2,1));
-    previousCity = L(Parents(2,1));
-    L(Parents(2,1)) = [];
-    for t = 2:size(Parents, 2)
-		edgeList(L(Parents(2,t)), 3) = previousCity;
-        edgeList(previousCity, 4)  = L(Parents(2, t));
-        previousCity = L(Parents(2,t));
-		L(Parents(2,t)) = [];
-        if size(L,2) == 0
-            edgeList(firstCity, 3) = previousCity;
-            edgeList(previousCity, 4) = firstCity;
-        end
-    end
+end
+
+function Offspring = create_child_ordinal(edgeList)
+    gens = size(edgeList, 1);
+	Offspring = zeros(1, gens);
     
     %select starting city
     currentCity = select_city(edgeList);
-    L = 1:cols;
+    L = 1:gens;
     
-    for i = 1:cols-1
+    for i = 1:gens-1
         for j = 1:size(L,2)
             if currentCity == L(j)
                 Offspring(i) = j;
@@ -60,10 +64,56 @@ function Offspring = edge_recombination(Parents, Repr)
         end
         edgeList(edgeList == currentCity) = 0;
         currentCity = select_city(edgeList, edgeList(currentCity, :));
-        
-        
     end
-    Offspring(cols) = 1;
-    Offspring = conv_repr(Offspring, 3, Repr);
+    Offspring(gens) = 1;
 end
-% End of function
+
+function edgeList = create_edgeList_adj(parent)
+    gens = size(parent, 2); 
+    edgeList = zeros(gens, 2);
+	edgeList(:,2) = parent';
+    for i = 1:gens
+        edgeList(parent(i),1) = i;
+    end
+end
+
+function Offspring = create_child_adj(edgeList)
+    gens = size(edgeList, 1);
+	Offspring = zeros(1, gens);
+    
+    firstCity = select_city(edgeList);
+    prevCity = firstCity;
+    edgeList(edgeList == prevCity) = 0;
+    for i = 1:gens-1
+        currentCity = select_city(edgeList, edgeList(prevCity, :));
+        edgeList(edgeList == currentCity) = 0;
+        Offspring(prevCity) = currentCity;
+        prevCity = currentCity;
+    end
+    Offspring(currentCity) = firstCity;
+end
+
+function edgeList = create_edgeList_path(parent)
+    gens = size(parent, 2); 
+    edgeList = zeros(gens, 2);
+    edgeList(1,1) = parent(gens);
+    edgeList(gens,2) = parent(1);
+    for i = 2:gens
+        edgeList(parent(i),1) = parent(i-1);
+        edgeList(parent(i-1),2) = parent(i);
+    end
+end
+
+function Offspring = create_child_path(edgeList)
+    gens = size(edgeList, 1);
+	Offspring = zeros(1, gens);
+    
+    %select starting city
+    currentCity = select_city(edgeList);
+    for i = 1:gens-1
+        edgeList(edgeList == currentCity) = 0;
+        Offspring(i) = currentCity;
+        currentCity = select_city(edgeList, edgeList(currentCity, :));
+    end
+    Offspring(gens) = currentCity;
+end
