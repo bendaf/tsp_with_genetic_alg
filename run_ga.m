d6bc8fa5cb7e91fa2f3f4ccaff6f09a40dd1a98d
bendaf <bendaf92@gmail.com>
2017-11-20 19:25:20 +0100

    Revert "no message"
    This reverts commit 75d4c466efe8f92736fbdec1550dcf5adfeb812e.

--- run_ga.m
+++ run_ga.m
@@ -1,9 +1,11 @@
-function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR_MUT, CROSSOVER, LOCALLOOP, ah1, ah2, ah3)
+function min_len = run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, ...
+    PR_CROSS, PR_MUT, CROSSOVER, MUTATION, LOCALLOOP, REPRESENTATION, TIME, ...
+    ah1, ah2, ah3 )
 % usage: run_ga(x, y, 
 %               NIND, MAXGEN, NVAR, 
 %               ELITIST, STOP_PERCENTAGE, 
-%               PR_CROSS, PR_MUT, CROSSOVER, 
-%               ah1, ah2, ah3)
+%               PR_CROSS, PR_MUT, CROSSOVER, MUTATION, REPRESENTATION,
+%               TIME, ah1, ah2, ah3)
 %
 %
 % x, y: coordinates of the cities
@@ -14,73 +16,76 @@ function run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR
 % PR_CROSS: probability for crossover
 % PR_MUT: probability for mutation
 % CROSSOVER: the crossover operator
+% MUTATION: the mutation operator
 % calculate distance matrix between each pair of cities
 % ah1, ah2, ah3: axes handles to visualise tsp
-{NIND MAXGEN NVAR ELITIST STOP_PERCENTAGE PR_CROSS PR_MUT CROSSOVER LOCALLOOP}
+% REPRESENTATION: Representation of the path.
+{NIND MAXGEN NVAR ELITIST STOP_PERCENTAGE PR_CROSS PR_MUT CROSSOVER ...
+    MUTATION LOCALLOOP REPRESENTATION};
 
-
+        if nargin < 17
+            useVisualisation = 0;
+            if nargin < 14
+                TIME = 60;
+            end
+        else
+            useVisualisation = 1;
+        end
+        tic
         GGAP = 1 - ELITIST;
-        mean_fits=zeros(1,MAXGEN+1);
-        worst=zeros(1,MAXGEN+1);
-        Dist=zeros(NVAR,NVAR);
-        for i=1:size(x,1)
-            for j=1:size(y,1)
-                Dist(i,j)=sqrt((x(i)-x(j))^2+(y(i)-y(j))^2);
+        mean_fits = zeros(1,MAXGEN+1);
+        worst = zeros(1,MAXGEN+1);
+        Dist = zeros(NVAR,NVAR);
+        for i = 1:size(x,1)
+            for j = 1:size(y,1)
+                Dist(i,j) = sqrt((x(i)-x(j))^2+(y(i)-y(j))^2);
             end
         end
         % initialize population
-        Chrom=zeros(NIND,NVAR);
-        for row=1:NIND
-        	%Chrom(row,:)=path2adj(randperm(NVAR));
-            Chrom(row,:)=randperm(NVAR);
+        Chrom = zeros(NIND,NVAR);
+        for row = 1:NIND
+            Chrom(row,:) = conv_repr(randperm(NVAR), 1, REPRESENTATION);
         end
-%         tour_size = 5*ones(NIND,1);
-%         Chrom = [Chrom tour_size];
-        
-        gen=0;
+        gen = 0;
         % number of individuals of equal fitness needed to stop
-        stopN=ceil(STOP_PERCENTAGE*NIND);
+        stopN = ceil(STOP_PERCENTAGE*NIND);
         % evaluate initial population
-        ObjV = tspfun(Chrom,Dist);
-        best=zeros(1,MAXGEN);
+        ObjV = tspfun(Chrom, Dist, REPRESENTATION);
+        best = zeros(1, MAXGEN);
+        min_len = min(ObjV);
         % generational loop
-        while gen<MAXGEN
-            sObjV=sort(ObjV);
-          	best(gen+1)=min(ObjV);
-        	minimum=best(gen+1);
-            mean_fits(gen+1)=mean(ObjV);
-            worst(gen+1)=max(ObjV);
-            for t=1:size(ObjV,1)
-                if (ObjV(t)==minimum)
-                    break;
-                end
+        while gen < MAXGEN
+            sObjV = sort(ObjV);
+          	[best(gen+1), t] = min(ObjV);
+            min_len = min(best(gen+1), min_len);
+            mean_fits(gen+1) = mean(ObjV);
+            worst(gen+1) = max(ObjV);
+            
+            if useVisualisation
+                visualizeTSP(x, y, conv_repr(Chrom(t, :), REPRESENTATION, 1), ...
+                    best(gen+1), ah1, gen, best, mean_fits, worst, ah2, ObjV, NIND, ah3);
             end
-            
-           % tour_size = Chrom(:,size(Chrom,2));
-            %Chrom = Chrom(:,1:size(Chrom,2)-1);
-            %visualizeTSP(x,y,adj2path(Chrom(t,:)), minimum, ah1, gen, best, mean_fits, worst, ah2, ObjV, NIND, ah3);
-            % if path representation is used, uncomment
-            visualizeTSP(x,y,Chrom(t,:), minimum, ah1, gen, best, mean_fits, worst, ah2, ObjV, NIND, ah3);
-            %Chrom = [Chrom tour_size];
-            
             if (sObjV(stopN)-sObjV(1) <= 1e-15)
                   break;
             end          
         	%assign fitness values to entire population
-        	FitnV=ranking(ObjV);
-        	%FitnV = FP(ObjV);
-            %select individuals for breeding
-        	SelCh=select('tour', Chrom, ObjV, GGAP);
+        	FitnV = ranking(ObjV);
+        	%select individuals for breeding
+        	SelCh = select('sus', Chrom, FitnV, GGAP);
         	%recombine individuals (crossover)
-            SelCh = recombin(CROSSOVER,SelCh,PR_CROSS);
-            SelCh=mutateTSP('insertion',SelCh,PR_MUT);
+            SelCh = feval(CROSSOVER, SelCh, PR_CROSS, REPRESENTATION);
+            SelCh = mutateTSP(MUTATION, SelCh, PR_MUT, REPRESENTATION);
             %evaluate offspring, call objective function
-        	ObjVSel = tspfun(SelCh,Dist);
+        	ObjVSel = tspfun(SelCh, Dist, REPRESENTATION);
             %reinsert offspring into population
-        	[Chrom ObjV]=reins(Chrom,SelCh,1,1,ObjV,ObjVSel);
+        	[Chrom ObjV] = reins(Chrom, SelCh, 1, 1, ObjV, ObjVSel);
             
-            Chrom = tsp_ImprovePopulation(NIND, NVAR, Chrom,LOCALLOOP,Dist);
+            Chrom = tsp_ImprovePopulation(NIND, NVAR, Chrom, LOCALLOOP, ...
+                Dist, REPRESENTATION);
         	%increment generation counter
-        	gen=gen+1;            
+        	gen = gen+1;
+            if toc > TIME
+                break;
+            end
         end
 end
