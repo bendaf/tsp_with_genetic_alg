84e3d7992c265aa30a7c77f2bad0ed6943e51bfa
saharch1990 <sahar.ch2017@gmail.com>
2017-11-21 15:46:11 +0100

    Insert new line

--- run_ga.m
+++ run_ga.m
@@ -67,6 +67,7 @@ function min_len = run_ga(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, ..
             end
             if (sObjV(stopN)-sObjV(1) <= 1e-15)
                   break;
+                  
             end          
         	%assign fitness values to entire population
         	FitnV = ranking(ObjV);
