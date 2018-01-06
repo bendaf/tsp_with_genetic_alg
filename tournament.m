00deb62a6754ba87c66e47dd2c195b8618e69823
bendaf <bendaf92@gmail.com>
2018-01-06 21:02:15 +0100

    Revert "changing tournament"
    This reverts commit 2b470dde378d12f705d1bf87c3e0b2c5f315e092.

--- tournament.m
+++ tournament.m
@@ -17,19 +17,23 @@
 %                The new population, ready for mating, can be obtained
 %                by calculating OldChrom(NewChrIx,:).
 
-function NewChrIx = tournament(FitnV,Nsel)
+function NewChrIx = tournament(FitnV,Nsel);
 
-tournSize = 5;
-N_ind=size(FitnV,1);
-ave=sum(1:N_ind)/N_ind;
+tournSize = 10;
+[Nind,~] = size(FitnV);
+NewChrIx = zeros(Nsel,1);
 
-for k = 1:Nsel
-    Random_selection=min(N_ind,ave+(N_ind-ave)*randn(1,tournSize));
-    selc = Random_selection(1);
-    for kk = 2:tournSize
-        if FitnV(Rsel(kk)) <= FitnV(selc)
-            selc = Random_selection(kk);
+for i = 1:Nsel
+    Rsel = randperm(Nind, tournSize);
+    best = Rsel(1);
+    for j = 2:tournSize
+        if FitnV(Rsel(j)) <= FitnV(best)
+            best = Rsel(j);
         end
     end
-    NewChrIx(k) = selc;
+    NewChrIx(i) = best;
 end
+        
+%[~, shuf] = sort(rand(Nsel, 1));
+NewChrIx = NewChrIx(randperm(Nsel));
+%NewChrIx = NewChrIx(shuf);
